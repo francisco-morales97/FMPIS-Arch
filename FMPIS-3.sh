4752fe83541157beb89ae1787dc9b8309df65815
fMorales-97 <fmorales12.97@gmail.com>
2023-11-27 21:41:46 -0300

    Actualizacion 27 nov

--- FMPIS-3.sh
+++ FMPIS-3.sh
@@ -29,26 +29,6 @@ case "$ANSWER" in
     chsh -s /bin/zsh
     echo
 
-    # Mueve archivos de configuración a sus ubicaciones correspondientes
-    printf "${GREEN}${BOLD}Instalando mis configuraciones${NC}\n"
-    sleep 1
-    mv configs/.zshenv ~/
-    mv configs/bin ~/.local/
-    mv -f configs/xdg_config_home/* ~/.config/
-    mv -f configs/xdg_data_home/* ~/.local/share/
-    if [ -e ~/.cache ]; then
-      mv -f configs/xdg_cache_home/* ~/.cache/
-    else
-      mkdir ~/.cache
-      mv -f configs/xdg_cache_home/* ~/.cache/
-    fi
-    echo
-
-    # Mueve mis wallpapers a la carpeta de imágenes
-    printf "${GREEN}${BOLD}Moviendo wallpapers${NC}\n"
-    sleep 1
-    mv configs/wallpapers/*.* ~/Imágenes
-    echo
     printf "${GREEN}${BOLD}La tercera parte del script ha finalizado${NC}\n"
     sleep 1
     printf "${GREEN}${BOLD}Reinicie la computadora para ver todos los cambios${NC}\n"
--- configs/xdg_config_home/nvim/lua/francisco/core/init.lua
+++
@@ -1,2 +0,0 @@
-require('francisco.core.options')
-require('francisco.core.keymaps')
